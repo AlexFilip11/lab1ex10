fbd95101adde0166d4b9b421f9bf6b007b45013d
Alex Filip <alexfilippaul@gmail.com>
2022-11-27 18:25:53 +0200

    Add project files.

---
+++ lab1ex10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lab1ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //suma cifrelor unui numar de 2 cifre
+            Console.WriteLine("Introduceti un numar de doua cifre: ");
+            int a = int.Parse(Console.ReadLine());
+            int suma = a % 10 + a / 10;
+            Console.WriteLine("Suma cifrelor numarului " + a + " este " + suma);
+        }
+    }
+}
